f85b493afa2ffc05b8e93f873a290c006a07a23b
Jan Schmitte <janschmitte@gmail.com>
2021-10-30 16:01:35 +0200

    script.py * added debug parameters * get Parameters from configuration files * get Save files from directories

--- scripts/script.py
+++ scripts/script.py
@@ -1,16 +1,37 @@
 import sys
 import getopt
+import configparser
+import os
+import glob
+
 
 def __main__(argv):
+    __init__()
+
+    getParameters(argv)
+    
+    if rom == '':
+        fillParametersForDebug()
+
+    getSaveDirectories()
+
+    getSaveFiles(system, savefile_directory, rom)
+
+    printDebug()
+
+
+def __init__():
+    global configBasePath
+
+    configBasePath = '/opt/retropie/configs/'
+
+
+def getParameters(argv):
     global system
     global emulator
     global rom
     global command
 
-    getParameters(argv)
-
-
-def getParameters(argv):
     system = ''
     emulator = ''
     rom = ''
@@ -19,7 +40,7 @@ def getParameters(argv):
     try:
         opts, args = getopt.getopt(argv, 'hs:e:r:c:', ['system=', 'emulator=', 'rom=', 'command='])
     except getopt.GetoptError:
-        print('FEHLER' + sys.stderr)
+        print('FEHLER')
         sys.exit(2)
 
     for opt, arg in opts:
@@ -37,17 +58,108 @@ def getParameters(argv):
         elif opt in ('-c', '--command'):
             command = arg
 
-    print('System: ', system)
-    print('Emulator: ', emulator)
-    print('ROM: ', rom)
-    print('Command: ', command)
 
-    f = open('/dev/shm/py.txt', 'w')
-    f.write('System: ' + system)
-    f.write('Emulator: ' + emulator)
-    f.write('ROM: ' + rom)
-    f.write('Command: ' + command)
-    f.close()
+def fillParametersForDebug():
+    global system
+    global emulator
+    global rom
+    global command
+
+    system = 'gb'
+    emulator = 'lr-gambatte'
+    rom = '/home/pi/RetroPie/roms/gb/Mystic Quest (Germany).zip'
+    command = '/opt/retropie/emulators/retroarch/bin/retroarch -L /opt/retropie/libretrocores/lr-gambatte/gambatte_libretro.so --config /opt/retropie/configs/gb/retroarch.cfg "/home/pi/RetroPie/roms/gb/Mystic Quest (Germany).zip'
+
+
+def getParameterFromConfig(system, parameter):
+    config = configparser.ConfigParser()
+
+    # Content
+    # TODO get parameter from content override
+
+    # Core
+    # TODO get parameter from core override
+
+    # System
+    try: 
+        f = open(configBasePath + system + '/retroarch.cfg')
+        c = '[dummy]\n' + f.read()
+
+        config.read_string(c)
+        retVal = config.get('dummy', parameter)
+
+        return retVal
+    except configparser.NoOptionError:
+        print('No system specific parameter found')
+
+    # All
+    try: 
+        f = open(configBasePath + 'all/retroarch.cfg')
+        c = '[dummy]\n' + f.read()
+
+        config.read_string(c)
+        retVal = config.get('dummy', parameter)
+
+        return retVal
+    except configparser.NoOptionError:
+        print('No default parameter found')
+
+    return 'n/a'
+
+
+def getSaveDirectories():
+    global savefile_directory
+    global savestate_directory
+
+    savefile_directory = getParameterFromConfig(system, 'savefile_directory')
+    if savefile_directory in ('n/a', '"default"'):
+        savefile_directory = os.path.dirname(rom)
+
+    savestate_directory = getParameterFromConfig(system, 'savestate_directory')
+    if savestate_directory in ('n/a', '"default"'):
+        savestate_directory = os.path.dirname(rom)
+
+
+def getSaveFiles(system, directory, rom):
+    config = configparser.ConfigParser()
+    config.read('/home/pi/RetroPie/scripts/cloud-sync/systems.ini')
+
+    global savefile_extensions
+    global savestate_extensions
+
+    savefile_extensions = config.get(system, 'savefile')
+    savestate_extensions = config.get(system, 'savestate')
+
+    romWithoutExt = rom.rsplit('.', 1)[0]
+    
+    global savefiles 
+    savefiles = set(glob.glob(romWithoutExt + '.' + savefile_extensions))
+
+    global savestates
+    savestates = set(glob.glob(romWithoutExt + '.' + savestate_extensions))
+
+    global savestates_images
+    savestates_images = set(glob.glob(romWithoutExt + '.' + savestate_extensions + '.png'))
+
+    savestates = set(savestates) - set(savestates_images)
+
+
+def printDebug():
+    print('----------')
+    print('System: ' + system)
+    print('Emulator: ' + emulator)
+    print('ROM: ' + rom)
+    print('Command: ' + command)
+    print('Savefile directory: ' + savefile_directory)
+    print('Savefile extensions: ' + savefile_extensions)
+    print('Savefiles:')
+    print(savefiles)
+    print('Savestate directory: ' + savestate_directory)
+    print('Savestate extensions: ' + savestate_extensions)
+    print('Savestates:')
+    print(savestates)
+    print('Savestate images:')
+    print(savestates_images)
 
 
 if __name__ == '__main__':
